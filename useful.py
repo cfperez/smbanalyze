9cdf0bd7a0a98d5f461c0353e4ca19a82a7157da
Christian Perez <cfperez@stanford.edu>
2012-09-15 12:09:29 -0700

    Improve dotdict class

--- useful.py
+++ useful.py
@@ -1,13 +1,26 @@
 def toNum(s):
-    try:
+  if s is None:
+	return None
+  try:
 	return int(s)
-    except ValueError:
+  except ValueError:
 	return float(s)
 
 
 class dotdict(dict):
-    def __getattr__(self, name):
+  def __setitem__(self, key, val):
+	if not key.replace('_','').isalnum():
+	  raise KeyError, "Key must be alphanumeric"
+	super(dotdict,self).__setitem__(key,val)
+
+  def __getitem__(self,key):
+	if not self.has_key(key):
+	  self[key] = dotdict()
+	return super(dotdict,self).__getitem__(key)
+
+  def __getattr__(self, name):
 	return self[name]
-    def __setattr__(self, name, value):
+
+  def __setattr__(self, name, value):
 	self[name] = value
 	return self
