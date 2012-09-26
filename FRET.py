4940646a98d520b6650a99f3d4176e424688095b
Christian Perez <cfperez@stanford.edu>
2012-09-26 14:55:50 -0700

    Add hist() plotting function; improvements and some refectoring to fromDirectory; remove Experiment test classes

--- FRET.py
+++ FRET.py
@@ -1,13 +1,21 @@
-import Image, FileIO
+import os
+import glob
+import re
+import operator
+
 import matplotlib.pyplot as plt
 from numpy import concatenate
-from FileIO import savedat,loaddat
-import os, glob, re, useful, operator
+
+import useful
+import Image
+import FileIO
 
 beta = 0.13
 gamma = 1.16
 
+molID = lambda t: 's{}m{}'.format(t.slide,t.mol)
 molname = lambda t: 's{}m{}_{}'.format(t.slide,t.mol,t.pull)
+pN = lambda f: 'f'+str(f)+'pN'
 
 def plot(*data, **kwargs):
   "loc=legend location (or None for off), title, hold (False for individual plots),\
@@ -15,12 +23,12 @@ def plot(*data, **kwargs):
   prefix=for all traces in legend"
 
   loc = kwargs.get('legend', 'best')
-  title = kwargs.get('title')
   hold = kwargs.get('hold',True)
 
   # to prefix labels in plots
   names = kwargs.get('names',[None]*len(data))
   prefix = kwargs.get('prefix','')
+  title = kwargs.get('title','')
 
   if len(names) != len(data):
 	raise ValueError, "Must have same number of names as traces to plot"
@@ -31,11 +39,10 @@ def plot(*data, **kwargs):
 	if not hold:
 	  plt.figure()
 
-	if hasfret:
+	if hasfret is not False:
 		plt.subplot(211)
 
-	if title:
-	  plt.title(title)
+	plt.title(title)
 
 	name = name or getattr(trace,'molname','')
 
@@ -53,7 +60,7 @@ def plot(*data, **kwargs):
 	if loc:
 	  plt.legend(loc=loc)
 
-	if hasfret:
+	if hasfret is not False:
 		plt.subplot(212)
 		plt.ylabel('FRET')
 		plt.xlabel('Frames')
@@ -66,13 +73,21 @@ def hist(*data, **kwargs):
   bins = kwargs.get('bins',50)
   attr = kwargs.get('plot','fret')
 
-  counts,bins,patches = \
-	plt.hist( concatenate(map(operator.attrgetter(attr),data)), bins )
+  #if len(data) == 1:
+  #  data = list(data[0])
+
+  if kwargs.get('join',True):
+    func = concatenate
+  else:
+    func = lambda x: x
+
+  counts,bins,patches = plt.hist(
+        func(map(operator.attrgetter(attr),data)), bins)
 
   delta = bins[1]-bins[0]
   bins = (bins-delta/2)[1:]
 
-  return counts,bins
+  return bins,counts
 
 def calc(stack, beta=beta, gamma=gamma):
     """Calculates FRET of a pull from an Image.Stack
@@ -92,7 +107,7 @@ def calcToFile(stack, filename, **kwargs):
     "saveFRETdata( fret, ImageStack, filename): saves donor,acceptor, FRET to 3 column text file"
 
     fretdata = calc(stack, **kwargs)
-    savedat(filename, (stack.time,stack.donor,stack.acceptor,fretdata), header='time donor acceptor FRET', fmt=('%u','%u','%.5f'))
+    FileIO.savedat(filename, (stack.time,stack.donor,stack.acceptor,fretdata), header='time donor acceptor FRET', fmt=('%.3f','%u','%u','%.5f'))
     return fretdata
 
 def fromDirectory(*args, **kwargs):
@@ -104,7 +119,7 @@ def fromDirectory(*args, **kwargs):
 	hold = 'singleplot' in args or kwargs.get('singleplot')
 	saveplot = 'saveplot' in args or kwargs.get('saveplot')
 	roi_file = kwargs.get('roi_file') # 'roi*' would be the convention
-	files = kwargs.get('glob','*.img')
+	file_glob = kwargs.get('files','*.img')
 	user_slide = kwargs.get('slide')
 	user_mol = kwargs.get('mol')
 	background = kwargs.get('background','')
@@ -112,59 +127,48 @@ def fromDirectory(*args, **kwargs):
 	old_dir = os.getcwd()
 	if dir:
 	  os.chdir(dir)
-
+    
 	try:
-	  roi_file = glob.glob(roi_file) if roi_file else None
 	  if roi_file:
-		roi = roi_file.pop()
 		Image.setDefaultROI(
-		  *Image.ROI.fromFile(roi, origin=kwargs.get('roi_origin','absolute')))
+		  *Image.ROI.fromFile(roi_file, origin=kwargs.get('roi_origin','absolute')))
+		if verbose:
+		  print "Using ROI file: %s" % roi_file
 
-	  if roi_file:
-		print "WARNING: Only using first ROI file found: %s" % roi_file
-	  elif verbose:
-		print "Using ROI file: %s" % roi_file
-	  elif not Image.Stack.defaultROI:
+	  if not Image.Stack.defaultROI:
 		raise RuntimeError, "No ROIs set or loaded--cannot compute counts"
 
-	  files = glob.glob( '*.img' )
+	  files = glob.glob(file_glob)
 	  bg_files = glob.glob( '*_background.img' )
 
 	  if verbose:
 		print "Found files:\n%s\n" % '\n'.join(files)
 
-	  slide_background = ''
-	  last_slide = None
-	  BG = None
-	  results = Experiments()
+	  slide_background = '' # last background used on this slide
+	  last_slide = None     # slide number of last file
+	  BG = None             # current background image
+	  results = useful.dotdict() # output
 
 	  for fname in files:
 
-	  # skip background files for FRET processing
+        # Skip background files for FRET processing
 		if fname in bg_files:
 		  continue
 
 		basename, ext = os.path.splitext(fname)
 
+        # Get molecule information from filename
 		finfo = FileIO.parseFilename(fname)
-		construct, context, slide, mol, pull, force, min, sec,\
-		  series, isBackground = finfo
+		(construct, context, slide, mol, pull, force, min, sec,
+            series, isBackground) = finfo
 
-		def match_or_included(x,y):
-		  if x is not None:
-			if type(x) is tuple:
-			  return y in x
-			else:
-			  return x==y
-		  return True
-
-		if not (match_or_included(user_slide,slide) or \
-			match_or_included(user_mol,mol)):
-		  print "skipping " + fname
+        # Only process requested slide and molecules
+		if (not match_or_included(user_slide,slide) or 
+		  not match_or_included(user_mol,mol)):
+		  if verbose:
+			print "skipping " + fname
 		  continue
 
-		# recursively search for background file with the most specific scope
-		# using '_' convention of file naming
 		def find_bg_filename(basename):
 			if basename == '':
 			  return ''
@@ -175,10 +179,15 @@ def fromDirectory(*args, **kwargs):
 			else:
 			  return find_bg_filename(basename.rpartition('_')[0])
 
+        # Look for background file using name heirarchy
 		bg_search = find_bg_filename(basename)
+
+        # Use the new background if a) on a new slide or b) it is more appropriate
 		if slide != last_slide or bg_search.count('_') >= background.count('_'):
 		  slide_background = background
+          # keep the last background if a new one is not found
 		  background = bg_search or background
+        # Use the last when when, e.g. s1m1_background and then s1m2
 		elif slide == last_slide and bg_search.count('_') < background.count('_'):
 		  background = slide_background
 
@@ -195,17 +204,17 @@ def fromDirectory(*args, **kwargs):
 
 		data = calcToFile( image, basename+'.fret' )
 
-		temp = Experiment(image=image, fret=data)
-
-		molID = 's%sm%s'%(slide,mol)
-		results[construct][molID][pull] = temp
+		temp = useful.dotdict(image=image, fret=data)
+		if force is not None:
+		  results[construct][molID(finfo)][pN(force)][series] = temp
+		else:
+		  results[construct][molID(finfo)][pull] = temp
 
 		last_slide = slide
 
 		if plotall:
-		  figure()
-		  title = ' '.join([construct, 's%sm%sp%s'%(slide,mol,pull)])
-		  plot(image, fret=data, title=title)
+		  plt.figure()
+		  plot(image, fret=data, title=' '.join([construct, molname(finfo)]))
 
 		  if saveplot:
 			plt.savefig('%s %s s%sm%s_%s.png'%(construct,context.replace('_',' '), \
@@ -214,86 +223,15 @@ def fromDirectory(*args, **kwargs):
 	finally:
 	  os.chdir(old_dir)
 
-	return results.__lock__()
+	results._lock()
+	return results
 
-class Exp2:
-    def __init__(self,*args,**kwargs):
-	self._mydict = dict(*args,**kwargs)
-
-    def __getitem__(self,name):
-	if name not in self._mydict and '=' not in name:
-	    self._mydict[name] = Exp2()
-	return self._mydict[name]
-
-    def __setitem__(self,name,val):
-	self._mydict[name]=val
-
-    def __setattr__(self,name,val):
-	if not name.startswith('_'):
-	    try:
-		a,b = re.split(r'[=,]',name)
-		name = name+a
-		if b:
-		    value=b
-	    except ValueError:
-		pass
-	    finally:
-		self.__setitem__(name,val)
-	else:
-	    self.__dict__[name] = val
-
-    def __getattr__(self,name):
-	if name in self._mydict:
-	    return self[name]
-	else:
-	    try:
-		return self.__dict__[name]
-	    except KeyError:
-		if name.startswith('_'):
-		    raise AttributeError, name
-		return self[name]
-
-    def __repr__(self):
-	return repr(self._mydict)
-
-
-class Experiments(dict):
-    def __init__(self,*args,**kwargs):
-	super(self.__class__,self).__init__(*args,**kwargs)
-	self._lock = False
-
-    def __getitem__(self,name):
-	if not self._lock:
-	    if type(name) == str and \
-		    name not in self and '=' not in name and '[' not in name:
-		self[name] = Experiments()
-	return dict.__getitem__(self, name)
-
-    def __getattr__(self, name):
-	if not name.startswith('_'):
-	    return self.__getitem__(name)
-	else:
-	    dict.__getattribute__(self,name)
-
-    def __setattr__(self, name, value):
-	if not name.startswith('_'):
-	    self[name] = value
-	else:
-	    super(self.__class__,self).__setattr__(name,value)
-
-    def __lock__(self):
-	self._lock = True
-	for item in self.items():
-	    if hasattr(item,'__lock__'):
-		item.__lock__()
-	return self
-
-class Experiment:
-    def __init__(self, **kwargs):
-	self.__dict__.update(kwargs)
-
-    def __setattr__(self, name, value):
-	raise AttributeError, "Instance of class %s is read-only" % self.__class__
-
-    def __repr__(self):
-	return 'Experiment: ' + str(self.__dict__)
+# recursively search for background file with the most specific scope
+# using '_' convention of file naming
+def match_or_included(x,y):
+  if x is not None:
+    if isinstance(x,tuple):
+      return y in x
+    else:
+      return x==y
+  return True
