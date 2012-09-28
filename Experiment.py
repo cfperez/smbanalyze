429b640b793fc60758b2e3b3800d6865ef3f989e
Christian Perez <cfperez@stanford.edu>
2012-09-28 15:53:51 -0700

    Feature/change: load() creates Pulling instance from multiple datasets

--- Experiment.py
+++ Experiment.py
@@ -1,6 +1,7 @@
 import os
 import collections
 import operator
+import glob
 
 import numpy as np
 
@@ -14,43 +15,47 @@ kT = lambda T: 0.0138965*T
 
 FretData = collections.namedtuple('FretData',('time','donor','acceptor','fret'))
 PullData = collections.namedtuple('PullData', ('ext','f','sep'))
+PullFretData = collections.namedtuple('PullingFretData',PullData._fields+FretData._fields)
 
 class ExperimentError(Exception):
   pass
 
-def load(filename, **kwargs):
+def load(fileglob, **kwargs):
   "Create a new Experiment subclass based on filetype and data"
   verbose = kwargs.get('verbose')
   recalc = kwargs.get('recalc')
 
-  basename,ext = os.path.splitext(filename)
-  if ext == '.str':
-	if verbose:
-	  print "Processing %s as pulling experiment..." % basename
+  fret = []
+  pulldata = []
+  finfo = []
 
-	fretfile = FileIO.add_fret_ext(basename)
-	if os.path.isfile(fretfile):
-	  if recalc:
-		imgfile = FileIO.add_img_ext(basename)
-		if verbose:
-		  print "Recalculating fret from %s" % imgfile
-		image = Image.Stack(imgfile)
-		fret = FRET.calcToFile(image,fretfile) 
-	  else:
-		if verbose:
-		  print "Loading fret from %s" % fretfile
-		fret = FileIO.loadFRET(fretfile)
-	else:
-		fret = None
+  files = glob.glob('*'+fileglob+'*.str')
+  for filename in files:
 
-	pullfile=FileIO.add_pull_ext(basename)
-	if verbose:
-	  print "Loading pulling data from %s" % pullfile
-	pulldata = FileIO.loadPull(pullfile)
-	
-	finfo = FileIO.parseFilename(filename)
+    basename,ext = os.path.splitext(filename)
+    fretfile = FileIO.add_fret_ext(basename)
+    if os.path.isfile(fretfile):
+      if recalc:
+        imgfile = FileIO.add_img_ext(basename)
+        if verbose:
+          print "Recalculating fret from %s" % imgfile
+        image = Image.Stack(imgfile)
+        fret += [FRET.calcToFile(image,fretfile)]
+      else:
+        if verbose:
+          print "Loading fret from %s" % fretfile
+        fret += [FileIO.loadFRET(fretfile)]
+    else:
+        fret += [None]
 
-	return Pulling(finfo,pulldata,fret)
+    pullfile = FileIO.add_pull_ext(basename)
+    if verbose:
+      print "Loading pulling data from %s" % pullfile
+    pulldata += [FileIO.loadPull(pullfile)]
+    
+    finfo += [FileIO.parseFilename(filename)]
+
+  return Pulling(finfo,pulldata,fret)
 
 
 def constants(**kwargs):
@@ -96,35 +101,29 @@ class Pulling(Base):
   # pull.plot(**kwargs) = sets up good defaults for title, names, etc.
   # pull.ext pull.f pull.sep pull.fret pull.donor
 
-  def __init__(self,finfo,pulls,fret=None):
+  def __init__(self,finfo,pulls,fret=[None]):
 	if type(finfo) is not list:
 	  finfo = [finfo]
 	  pulls = [pulls]
 
-	super(Pulling,self).__init__(finfo[0])#,fret,pulls)
-	self.pull = None
-
 	self.size = len(pulls)
 
 	if fret is not None and len(fret) != self.size:
-	  raise ExperimentError, "Must have equal number of pulling curves and fret traces"
+	  raise ExperimentError("Must have equal number of pulling curves and fret traces")
+
+	super(Pulling,self).__init__(finfo[0])#,fret,pulls)
+	self.pull = None
 
 	self._pulls = pulls
 	self._fret = fret
 	self.info = finfo
 
-	if fret:
-	  self.datatype = \
-		collections.namedtuple('PullingFretData',PullData._fields+FretData._fields)
-	else:
-	  self.datatype = PullData
-
   def __getitem__(self,key):
-	if self._fret:
-	  #return self.datatype(*(self.pulls[key]+self.fret[key]))
-	  return Base(self.info[key], self._pulls[key], self._fret[key])
+	if self._fret[key]:
+	  return PullFretData(*(self._pulls[key]+self._fret[key]))
+	  #return Base(self.info[key], self._pulls[key], self._fret[key])
 	else:
-	  return self.datatype(*self._pulls[key])
+	  return PullData(*self._pulls[key])
 
   def __len__(self):
 	return self.size
