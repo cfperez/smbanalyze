e00072513f2dd6b244c72034486769a7e440f6e4
cfperez <cfperez@stanford.edu>
2012-10-15 15:46:56 -0700

    FIXED: indention

--- Experiment.py
+++ Experiment.py
@@ -85,8 +85,8 @@ def load(fileglob, **kwargs):
 
 def constants(**kwargs):
   for (key,val) in kwargs.iteritems():
-	if hasattr(Base,key):
-	  pass
+    if hasattr(Base,key):
+      pass
 
 class Container(object):
   pass
@@ -96,24 +96,24 @@ class Base(object):
   # also classmethods which change experimental constants like bead radii,
   # trap stiffnesses, etc.
   def __init__(self,*args):#,fret,fext):
-	self.fieldnames = ()
-	for arg in args:
-	  self._setattr(arg)
+    self.fieldnames = ()
+    for arg in args:
+      self._setattr(arg)
 
   def _setattr(self,named):
-	for attr,val in named._asdict().iteritems():
-	  self.fieldnames += (attr,)
-	  setattr(self, attr, val)
+    for attr,val in named._asdict().iteritems():
+      self.fieldnames += (attr,)
+      setattr(self, attr, val)
 
   def plot(self):
-	raise NotImplementedError
+    raise NotImplementedError
 
   def plot(self):
-	raise NotImplementedError
+    raise NotImplementedError
 
   @property
   def molname(self):
-	return FRET.molname(self)
+    return FRET.molname(self)
 
 class Pulling(Base):
   "stretching curves of single molecule .str camera .cam image .img"
@@ -127,46 +127,46 @@ class Pulling(Base):
   # pull.ext pull.f pull.sep pull.fret pull.donor
 
   def __init__(self,finfo,pulls,fret=[None]):
-	if not isinstance(finfo,list):
-	  finfo = [finfo]
-	  pulls = [pulls]
+    if not isinstance(finfo,list):
+      finfo = [finfo]
+      pulls = [pulls]
 
-	self.size = len(pulls)
+    self.size = len(pulls)
 
-	if fret != [None] and len(fret) != self.size:
-	  raise ExperimentError("Must have equal number of pulling curves and fret traces")
+    if fret != [None] and len(fret) != self.size:
+      raise ExperimentError("Must have equal number of pulling curves and fret traces")
 
-	super(Pulling,self).__init__(finfo[0])#,fret,pulls)
-	self.pull = None
+    super(Pulling,self).__init__(finfo[0])#,fret,pulls)
+    self.pull = None
 
-	self._pulls = pulls
-	self._fret = fret
-	self.info = finfo
+    self._pulls = pulls
+    self._fret = fret
+    self.info = finfo
 
   def __getitem__(self,key):
-	if self._fret[key] is not None:
-	  return PullFretData(*(self._pulls[key]+self._fret[key]))
-	  #return Base(self.info[key], self._pulls[key], self._fret[key])
-	else:
-	  return PullData(*self._pulls[key])
+    if self._fret[key] is not None:
+      return PullFretData(*(self._pulls[key]+self._fret[key]))
+      #return Base(self.info[key], self._pulls[key], self._fret[key])
+    else:
+      return PullData(*self._pulls[key])
 
   def __len__(self):
-	return self.size
+    return self.size
 
   def __iter__(self):
-	for i in range(self.size):
-	  yield self[i]
+    for i in range(self.size):
+      yield self[i]
 
   @property
   def molname(self):
-	return 's{}m{}'.format(self.slide,self.mol)
+    return 's{}m{}'.format(self.slide,self.mol)
 
   @property
   def fret(self):
-	return np.concatenate(map(operator.attrgetter('fret'),self._fret))
+    return np.concatenate(map(operator.attrgetter('fret'),self._fret))
 
   def _concat_property(self, name):
-	return map( operator.attrgetter(name), getattr(self,name))
+    return map( operator.attrgetter(name), getattr(self,name))
 
 class OpenLoop(Base):
   "camera .cam image. img"
