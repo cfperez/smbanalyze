472ca17c139e7dbf541024a210312d765adda143
cfperez <cfperez@stanford.edu>
2013-01-25 11:18:53 -0800

    ADDED _hasData and has___Data functions for testing if objects contain data of given kind

--- Types.py
+++ Types.py
@@ -1,5 +1,18 @@
+from numpy import all
+
 import collections
 
-FretData = collections.namedtuple('FretData',('time','donor','acceptor','fret'))
-PullData = collections.namedtuple('PullData', ('ext','f','sep'))
-PullFretData = collections.namedtuple('PullingFretData',PullData._fields+FretData._fields)
+FretData_fields = ('time','donor','acceptor','fret')
+PullData_fields = ('ext','f','sep')
+
+FretData = collections.namedtuple('FretData', FretData_fields)
+PullData = collections.namedtuple('PullData', PullData_fields)
+PullFretData = collections.namedtuple('PullingFretData',PullData_fields+FretData_fields)
+
+def _hasData(datatype):
+  fields = getattr(datatype,'_fields',datatype)
+  return lambda obj: all(map(hasattr,[obj]*len(fields),fields))
+
+hasFretData = _hasData(FretData)
+hasPullData = _hasData(PullData)
+hasPullFretData = _hasData(PullFretData)
