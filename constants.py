ec103545e94c6dbcc93a2fb7c97b3ce25cb3db3c
Christian Perez <christian.f.perez@gmail.com>
2014-05-01 14:13:15 -0700

    Update constants

--- constants.py
+++ constants.py
@@ -1,16 +1,16 @@
 import logging
 
-parameters = {'T':293.2}
 
 temperature = 24.0
+parameters = {'T':273.2 + temperature}
 
 kT = lambda T: 0.0138965*T
 
 beta = 0.13
-gamma = 1.11
+gamma = 1.
 
-sumOfBeadRadii = 715
-stiffness = (1.76, 0.31)
+sumOfBeadRadii = 710
+stiffness = (1.57, 0.275)
 
 default_pulling_sampling_time = 0.01
 default_fret_exposure_time_ms = 30
